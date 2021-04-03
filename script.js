b38d7841a2b0a00936f71c4cb6009181b7e695ec
LuzVictoria <luzaldanavictoria@gmail.com>
2021-04-03 11:29:55 -0300

    contador de tareas, lista de tareas y checkbox

--- script.js
+++ script.js
@@ -3,13 +3,43 @@ const classNames = {
     TODO_CHECKBOX: 'todo-checkbox',
     TODO_TEXT: 'todo-text',
     TODO_DELETE: 'todo-delete',
-  }
-  
-  
-  const list = document.getElementById('todo-list')
-  const itemCountSpan = document.getElementById('item-count')
-  const uncheckedCountSpan = document.getElementById('unchecked-count')
-  
-  function addTodo() {
-    alert('Boton Add TODO clickeado!')
-  }
+}
+
+
+const list = document.getElementById('todo-list')
+const itemCountSpan = document.getElementById('item-count')
+const uncheckedCountSpan = document.getElementById('unchecked-count')
+
+let contador = 0
+
+
+function addTodo() {
+
+    const tituloTarea = prompt('Que tarea desea agregar?')
+    if (tituloTarea != undefined) {
+        contador++
+        itemCountSpan.innerHTML = contador
+        const item = document.createElement('li')
+        item.innerHTML = tituloTarea
+        const checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        checkbox.value = 1;
+        checkbox.name = "todo[]";
+        list.appendChild(item)
+        item.appendChild(checkbox)
+
+
+    }
+}
+
+
+
+function checkedItem() { // esta funcion esta sin terminar,seria la funcion que recorre la lista completa y dsp deberia contar cuantas tareas tengo pendientes
+    let contador = 0;
+    for (let index = 0; index < list.length; index++) {
+        console.log(list[index])
+            // if(list[index])
+
+    }
+
+}
