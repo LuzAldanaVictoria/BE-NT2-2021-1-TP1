2faf327b91fb32f8e5cb501c504cb71268bec892
demianv-ch <71720609+demianv-ch@users.noreply.github.com>
2021-04-05 19:49:53 -0300

    Checked

--- script.js
+++ script.js
@@ -4,42 +4,53 @@ const classNames = {
     TODO_TEXT: 'todo-text',
     TODO_DELETE: 'todo-delete',
 }
-
-
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
-let contador = 0
+let contador = 0;
+let resueltos = 0;
 
 
 function addTodo() {
-
     const tituloTarea = prompt('Que tarea desea agregar?')
-    if (tituloTarea != undefined) {
+    if (tituloTarea != undefined && tituloTarea != '') {
         contador++
         itemCountSpan.innerHTML = contador
         const item = document.createElement('li')
         item.innerHTML = tituloTarea
         const checkbox = document.createElement('input');
         checkbox.type = "checkbox";
-        checkbox.value = 1;
-        checkbox.name = "todo[]";
+        checkbox.name = "todo";
+        checkbox.value = contador;
+        resueltos++;
+
+        checkbox.addEventListener("click", checkedItem);
+
         list.appendChild(item)
         item.appendChild(checkbox)
+        function checkedItem(e) { // esta funcion esta sin terminar,seria la funcion que recorre la lista completa y dsp deberia contar cuantas tareas tengo pendientes
+            let res = 0;
+            const checkeados = document.querySelectorAll(`input[name="todo"]:checked`)
+            checkeados.forEach((chekbox) => {
+                res++;
+            })
+            uncheckedCountSpan.innerHTML = resueltos - res;
+            console.log(resueltos - res)
 
-
+            /* var inputs = document.getElementsByTagName('input');
+            for (var i = 0; i < inputs.length; i++) {
+                if(inputs[i].checked)
+                
+                if (inputs[i] == 'checkbox' && inputs[i].checked()) {
+                    resuelto++;
+                }
+                console.log(inputs[i].value)
+            }
+            let porHacer = contador - resuelto;
+            console.log(porHacer)
+            uncheckedCountSpan.innerHTML = porHacer; */
+        }
     }
-}
-
-
-
-function checkedItem() { // esta funcion esta sin terminar,seria la funcion que recorre la lista completa y dsp deberia contar cuantas tareas tengo pendientes
-    let contador = 0;
-    for (let index = 0; index < list.length; index++) {
-        console.log(list[index])
-            // if(list[index])
-
-    }
-
+    checkedItem();
 }
